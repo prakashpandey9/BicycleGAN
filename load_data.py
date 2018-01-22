4db7b57cc06cf08f57223f8447da4a4c3813694a
Prakash Pandey <pandeyprakash987@gmail.com>
2018-01-23 01:40:48 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,40 +1,44 @@
 import numpy as np
 import os
 import h5py
-from glob import glob
+import glob
 import scipy.misc
 from scipy.misc import imread, imresize
 
-def load_images(dir):
-	all_images = []
-	for sub_dir in ['train', 'val']:
-		folder_path = os.path.join(dir, sub_dir)
-		all_img_A = []
-		all_img_B = []
-		for path in glob(os.path.join(folder_path, '*.jpg')):
-			full_image = imread(path)
-			img_A = full_image[:, :full_image.shape[1]/2, :]
-			img_B = full_image[:, full_image.shape[1]/2:, :]
-			all_img_A.append(img_A)
-			all_img_B.append(img_B)
+def load_images():
+	train_all=glob.glob("cityscapes/train/*.jpg")
+	train_img_A = []
+	train_img_B = []
 
-		#all_images[(sub_dir+'A')] = img_A
-		#all_images[(sub_dir+'B')] = img_B
-		all_images.append((sub_dir+'A', all_img_A))
-		all_images.append((sub_dir+'B', all_img_B))
+	for file in train_all:
+		full_image = imread(file)
+		img_A = full_image[:, :full_image.shape[1]/2, :]
+	 	img_B = full_image[:, full_image.shape[1]/2:, :]
+	 	train_img_A.append(img_A)
+	 	train_img_B.append(img_B)
 
-	all_images = load_images('dir')
-	train_A = []
-	train_B = []
-	val_A = []
-	val_B = []
+	train_A = np.asarray(train_img_A)
+	train_B = np.asarray(train_img_B)
+	print train_A.shape
+	print train_B.shape
 
-	train_A = np.asarray(all_images[0][1])
-	train_B = np.asarray(all_images[1][1])
-	val_A = np.asarray(all_images[2][1])
-	val_B = np.asarray(all_images[3][1])
+	test_all = glob.glob("cityscapes/val/*.jpg")
+	test_img_A = []
+	test_img_B = []
 
-	return train_A, train_B, val_A, val_B
+	for file in test_all:
+		full_image = imread(file)
+		img_A = full_image[:, :full_image.shape[1]/2, :]
+	 	img_B = full_image[:, full_image.shape[1]/2:, :]
+	 	test_img_A.append(img_A)
+	 	test_img_B.append(img_B)
+
+	test_A = np.asarray(test_img_A)
+	test_B = np.asarray(test_img_B)
+	print test_A.shape
+	print test_B.shape
+	
+	return train_A, train_B, test_A, test_B
 
 def save_images(images, size, image_path):
 	return imsave(inverse_transform(images), size, image_path)
